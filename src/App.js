aef899b1e80ffdf8a5bfc60cbfa814378214409c
ahyaralfaruq <ahyaralfaruq97@gmail.com>
2023-06-22 09:07:02 +0700

    fixing import images

--- src/App.js
+++ src/App.js
@@ -1,30 +1,49 @@
-import React from 'react';
-import {
-  SectionBackground,
-  SectionQuotes,
-  SectionGroom,
-  SectionLocations,
-  SectionGift,
-  SectionPhotos,
-} from './components';
+import React, { Suspense, lazy } from 'react';
+// import {
+//   SectionBackground,
+//   SectionQuotes,
+//   SectionGroom,
+//   SectionLocations,
+//   SectionGift,
+//   SectionPhotos,
+// } from './components';
 
 import './App.css';
+
+const SectionBackground = lazy(() => import("./components/SectionBackground"))
+const SectionQuotes = lazy(() => import("./components/SectionQuotes"))
+const SectionGroom = lazy(() => import("./components/SectionGroom"))
+const SectionLocations = lazy(() => import("./components/SectionLocations"))
+const SectionGift = lazy(() => import("./components/SectionGift"))
+const SectionPhotos = lazy(() => import("./components/SectionPhotos"))
 
 function App() {
   return (
     <div className="App">
       {/* section background wedding invitation */}
-      <SectionBackground />
+      <Suspense fallback={<div>Loading...</div>}>
+        <SectionBackground />
+      </Suspense>
       {/* section pearl words */}
-      <SectionQuotes />
+      <Suspense fallback={<div>Loading...</div>}>
+        <SectionQuotes />
+      </Suspense>
       {/* section groom and bride */}
-      <SectionGroom />
+      <Suspense fallback={<div>Loading...</div>}>
+        <SectionGroom />
+      </Suspense>
       {/* section wedding location */}
-      <SectionLocations />
+      <Suspense fallback={<div>Loading...</div>}>
+        <SectionLocations />
+      </Suspense>
       {/* section photos */}
-      <SectionPhotos />
+      <Suspense fallback={<div>Loading...</div>}>
+        <SectionPhotos />
+      </Suspense>
       {/* section send gift */}
-      <SectionGift />
+      <Suspense fallback={<div>Loading...</div>}>
+        <SectionGift />
+      </Suspense>
     </div>
   );
 }
