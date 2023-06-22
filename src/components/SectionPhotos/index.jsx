4d290439803b40e7ccd952e4592413176c1b5bde
ahyaralfaruq <ahyaralfaruq97@gmail.com>
2023-06-22 08:48:03 +0700

    fixing import images

--- src/components/SectionPhotos/index.jsx
+++ src/components/SectionPhotos/index.jsx
@@ -1,69 +1,69 @@
 import React, { memo } from 'react'
-import ImagesData from "../../utils/images"
+// import ImagesData from "../../utils/images"
 
 const SectionPhotos = () => {
-  // const imagesData = [
-  //   {
-  //     id: 1,
-  //     src: "assets/images/background.JPG"
-  //   },
-  //   {
-  //     id: 2,
-  //     src: "assets/images/groom.JPG"
-  //   },
-  //   {
-  //     id: 3,
-  //     src: "assets/images/bride.JPG"
-  //   },
-  //   {
-  //     id: 4,
-  //     src: "assets/images/1.JPG"
-  //   },
-  //   {
-  //     id: 5,
-  //     src: "assets/images/2.JPG"
-  //   },
-  //   {
-  //     id: 6,
-  //     src: "assets/images/3.JPG"
-  //   },
-  //   {
-  //     id: 7,
-  //     src: "assets/images/4.JPG"
-  //   },
-  //   {
-  //     id: 8,
-  //     src: "assets/images/5.JPG"
-  //   },
-  //   {
-  //     id: 9,
-  //     src: "assets/images/6.JPG"
-  //   },
-  //   {
-  //     id: 10,
-  //     src: "assets/images/7.JPG"
-  //   },
-  //   {
-  //     id: 11,
-  //     src: "assets/images/8.JPG"
-  //   },
-  //   {
-  //     id: 12,
-  //     src: "assets/images/9.JPG"
-  //   },
-  //   {
-  //     id: 13,
-  //     src: "assets/images/10.JPG"
-  //   },
-  //   {
-  //     id: 14,
-  //     src: "assets/images/11.JPG"
-  //   },
-  //   {
-  //     id: 15,
-  //     src: "assets/images/12.JPG"
-  //   },
-  // ]
+  const imagesData = [
+    {
+      id: 1,
+      src: "assets/images/background.JPG"
+    },
+    {
+      id: 2,
+      src: "assets/images/groom.JPG"
+    },
+    {
+      id: 3,
+      src: "assets/images/bride.JPG"
+    },
+    {
+      id: 4,
+      src: "assets/images/1.JPG"
+    },
+    {
+      id: 5,
+      src: "assets/images/2.JPG"
+    },
+    {
+      id: 6,
+      src: "assets/images/3.JPG"
+    },
+    {
+      id: 7,
+      src: "assets/images/4.JPG"
+    },
+    {
+      id: 8,
+      src: "assets/images/5.JPG"
+    },
+    {
+      id: 9,
+      src: "assets/images/6.JPG"
+    },
+    {
+      id: 10,
+      src: "assets/images/7.JPG"
+    },
+    {
+      id: 11,
+      src: "assets/images/8.JPG"
+    },
+    {
+      id: 12,
+      src: "assets/images/9.JPG"
+    },
+    {
+      id: 13,
+      src: "assets/images/10.JPG"
+    },
+    {
+      id: 14,
+      src: "assets/images/11.JPG"
+    },
+    {
+      id: 15,
+      src: "assets/images/12.JPG"
+    },
+  ]
 
   return (
     <section id="sectionAlbum">
@@ -72,8 +72,8 @@ const SectionPhotos = () => {
 
         <div className="grid xl:grid-cols-4 xl:gap-4 md:grid-cols-3 sm:grid-cols-2 sm:gap-4">
           {
-            ImagesData.map( res => (
-              <div className="w-full overflow-hidden rounded-md" key={res.id}>
+            imagesData.map( (res, i) => (
+              <div className="w-full overflow-hidden rounded-md" key={i}>
                 <img src={res.src} alt="gambar" className='w-full object-cover' />
               </div>
             ))
