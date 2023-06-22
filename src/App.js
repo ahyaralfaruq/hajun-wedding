65a274cd778425ba6b197e6052c6e0f8ec13a028
ahyaralfaruq <ahyaralfaruq97@gmail.com>
2023-06-22 09:16:58 +0700

    fixing import images

--- src/App.js
+++ src/App.js
@@ -1,49 +1,30 @@
-import React, { Suspense, lazy } from 'react';
-// import {
-//   SectionBackground,
-//   SectionQuotes,
-//   SectionGroom,
-//   SectionLocations,
-//   SectionGift,
-//   SectionPhotos,
-// } from './components';
+import React from 'react';
+import {
+  SectionBackground,
+  SectionQuotes,
+  SectionGroom,
+  SectionLocations,
+  SectionGift,
+  SectionPhotos,
+} from './components';
 
 import './App.css';
-
-const SectionBackground = lazy(() => import("./components/SectionBackground"))
-const SectionQuotes = lazy(() => import("./components/SectionQuotes"))
-const SectionGroom = lazy(() => import("./components/SectionGroom"))
-const SectionLocations = lazy(() => import("./components/SectionLocations"))
-const SectionGift = lazy(() => import("./components/SectionGift"))
-const SectionPhotos = lazy(() => import("./components/SectionPhotos"))
 
 function App() {
   return (
     <div className="App">
       {/* section background wedding invitation */}
-      <Suspense fallback={<div>Loading...</div>}>
-        <SectionBackground />
-      </Suspense>
+      <SectionBackground />
       {/* section pearl words */}
-      <Suspense fallback={<div>Loading...</div>}>
-        <SectionQuotes />
-      </Suspense>
+      <SectionQuotes />
       {/* section groom and bride */}
-      <Suspense fallback={<div>Loading...</div>}>
-        <SectionGroom />
-      </Suspense>
+      <SectionGroom />
       {/* section wedding location */}
-      <Suspense fallback={<div>Loading...</div>}>
-        <SectionLocations />
-      </Suspense>
+      <SectionLocations />
       {/* section photos */}
-      <Suspense fallback={<div>Loading...</div>}>
-        <SectionPhotos />
-      </Suspense>
+      <SectionPhotos />
       {/* section send gift */}
-      <Suspense fallback={<div>Loading...</div>}>
-        <SectionGift />
-      </Suspense>
+      <SectionGift />
     </div>
   );
 }
--- src/components/SectionPhotos/index.jsx
+++ src/components/SectionPhotos/index.jsx
@@ -1,5 +1,5 @@
 import React, { memo } from 'react'
-import ImagesData from "../../utils/images"
+// import ImagesData from "../../utils/images"
 
 const SectionPhotos = () => {
 
@@ -9,13 +9,51 @@ const SectionPhotos = () => {
         <h3 className='mb-12 font-normal xl:text-2xl md:text-xl sm:text-lg text-center'>Gallery</h3>
 
         <div className="grid xl:grid-cols-4 xl:gap-4 md:grid-cols-3 sm:grid-cols-2 sm:gap-4">
-          {
-            ImagesData.map( res => (
-              <div className="w-full overflow-hidden rounded-md" key={res.id}>
-                <img src={res.src} alt="gambar" className='w-full object-cover' />
-              </div>
-            ))
-          }
+          <div className="w-full overflow-hidden rounded-md">
+            <img src="assets/images/background.jpg" alt="gambar" className='w-full object-cover' />
+          </div>
+          <div className="w-full overflow-hidden rounded-md">
+            <img src="assets/images/groom.jpg" alt="gambar" className='w-full object-cover' />
+          </div>
+          <div className="w-full overflow-hidden rounded-md">
+            <img src="assets/images/bride.jpg" alt="gambar" className='w-full object-cover' />
+          </div>
+          <div className="w-full overflow-hidden rounded-md">
+            <img src="assets/images/1.jpg" alt="gambar" className='w-full object-cover' />
+          </div>
+          <div className="w-full overflow-hidden rounded-md">
+            <img src="assets/images/2.jpg" alt="gambar" className='w-full object-cover' />
+          </div>
+          <div className="w-full overflow-hidden rounded-md">
+            <img src="assets/images/3.jpg" alt="gambar" className='w-full object-cover' />
+          </div>
+          <div className="w-full overflow-hidden rounded-md">
+            <img src="assets/images/4.jpg" alt="gambar" className='w-full object-cover' />
+          </div>
+          <div className="w-full overflow-hidden rounded-md">
+            <img src="assets/images/5.jpg" alt="gambar" className='w-full object-cover' />
+          </div>
+          <div className="w-full overflow-hidden rounded-md">
+            <img src="assets/images/6.jpg" alt="gambar" className='w-full object-cover' />
+          </div>
+          <div className="w-full overflow-hidden rounded-md">
+            <img src="assets/images/7.jpg" alt="gambar" className='w-full object-cover' />
+          </div>
+          <div className="w-full overflow-hidden rounded-md">
+            <img src="assets/images/8.jpg" alt="gambar" className='w-full object-cover' />
+          </div>
+          <div className="w-full overflow-hidden rounded-md">
+            <img src="assets/images/9.jpg" alt="gambar" className='w-full object-cover' />
+          </div>
+          <div className="w-full overflow-hidden rounded-md">
+            <img src="assets/images/10.jpg" alt="gambar" className='w-full object-cover' />
+          </div>
+          <div className="w-full overflow-hidden rounded-md">
+            <img src="assets/images/11.jpg" alt="gambar" className='w-full object-cover' />
+          </div>
+          <div className="w-full overflow-hidden rounded-md">
+            <img src="assets/images/12.jpg" alt="gambar" className='w-full object-cover' />
+          </div>
         </div>
       </div>
     </section>
